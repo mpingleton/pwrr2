48939cfc772e416e6aa06b5420b4fef92fd4acc3
Michael Pingleton <mnpingleton@gmail.com>
2022-09-17 18:22:55 +0300

    Added list and content view to projects and tasks pages.

--- pwrr2-ui/src/pages/Projects.jsx
+++ pwrr2-ui/src/pages/Projects.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import ListAndContentView from '../layouts/ListAndContentView';
 
 function Projects() {
 
+    return (
+        <ListAndContentView />
+    );
 }
 
 export default Projects;
--- pwrr2-ui/src/pages/Tasks.jsx
+++ pwrr2-ui/src/pages/Tasks.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import ListAndContentView from '../layouts/ListAndContentView';
 
 function Tasks() {
 
+    return (
+        <ListAndContentView />
+    );
 }
 
 export default Tasks;
