88572026ded04a6e3b1f7b6633dc4a2bda6cbc27
Michael Pingleton <mnpingleton@gmail.com>
2022-09-20 16:21:16 +0300

    Split up the stages into individual functions.

--- pwrr2-ui/src/components/ProjectStagePanel.jsx
+++ pwrr2-ui/src/components/ProjectStagePanel.jsx
@@ -12,10 +12,115 @@ import Stream from '@mui/icons-material/Stream';
 import Pending from '@mui/icons-material/PendingOutlined';
 
 function ProjectStagePanel(props) {
+
+    const submitted = () => {
+        return (
+            <Stack
+                direction="row"
+                spacing={1}
+                justifyContent='space-between'
+                alignItems='center'
+            >
+                <Done />
+                <Typography variant='subtitle1'>Submitted</Typography>
+                <Stack
+                    direction="column"
+                    alignItems='center'
+                >
+                    <Typography variant='subtitle2'>31 Jan 2022</Typography>
+                </Stack>
+            </Stack>
+        );
+    };
+
+    const validated = () => {
+        return (
+            <Stack
+                direction="row"
+                spacing={1}
+                justifyContent='space-between'
+                alignItems='center'
+            >
+                <Done />
+                <Typography variant='subtitle1'>Validated</Typography>
+                <Stack
+                    direction="column"
+                    alignItems='center'
+                >
+                    <Typography variant='subtitle2'>31 Jan 2022</Typography>
+                </Stack>
+                
+            </Stack>
+        );
+    };
+
+    const planned = () => {
+        return (
+            <Stack
+                direction="row"
+                spacing={1}
+                justifyContent='space-between'
+                alignItems='center'
+            >
+                <Stream />
+                <Typography variant='subtitle1'>Planning</Typography>
+                <Stack
+                    direction="column"
+                    alignItems='center'
+                >
+                    <Typography variant='subtitle2'>Due</Typography>
+                    <Typography variant='subtitle2'>31 Jan 2022</Typography>
+                </Stack>
+            </Stack>
+        );
+    };
+
+    const implemented = () => {
+        return (
+            <Stack
+                direction="row"
+                spacing={1}
+                justifyContent='space-between'
+                alignItems='center'
+            >
+                <Pending />
+                <Typography variant='subtitle1'>Implementation</Typography>
+                <Stack
+                    direction="column"
+                    alignItems='center'
+                >
+                    <Typography variant='subtitle2'>Due</Typography>
+                    <Typography variant='subtitle2'>31 Jan 2022</Typography>
+                </Stack>
+            </Stack>
+        );
+    };
+
+    const completed = () => {
+        return (
+            <Stack
+                direction="row"
+                spacing={1}
+                justifyContent='space-between'
+                alignItems='center'
+            >
+                <Pending />
+                <Typography variant='subtitle1'>Completion</Typography>
+                <Stack
+                    direction="column"
+                    alignItems='center'
+                >
+                    <Typography variant='subtitle2'>Due</Typography>
+                    <Typography variant='subtitle2'>31 Jan 2022</Typography>
+                </Stack>
+            </Stack>
+        );
+    };
+
     return (
         <Paper
             sx={{
-                width: '60%',
+                width: '50%',
                 height: '100%',
                 boxSizing: 'border-box',
                 padding: 2,
@@ -29,7 +134,7 @@ function ProjectStagePanel(props) {
                     variant='contained'
                     sx={{ width: '100%' }}
                 >
-                    Complete Project
+                    Complete
                 </Button>
                 <Stack
                     direction="row"
@@ -45,93 +150,19 @@ function ProjectStagePanel(props) {
                         variant="contained"
                         sx={{ width: '100%' }}
                     >
-                        Cancel Project
+                        Cancel
                     </Button>
                 </Stack>
                 <Divider />
-                <Stack
-                    direction="row"
-                    spacing={1}
-                    justifyContent='space-between'
-                    alignItems='center'
-                >
-                    <Done />
-                    <Typography variant='subtitle1'>Submitted</Typography>
-                    <Stack
-                        direction="column"
-                        alignItems='center'
-                    >
-                        <Typography variant='subtitle2'>31 Jan 2022</Typography>
-                    </Stack>
-                </Stack>
+                {submitted()}
                 <Divider />
-                <Stack
-                    direction="row"
-                    spacing={1}
-                    justifyContent='space-between'
-                    alignItems='center'
-                >
-                    <Done />
-                    <Typography variant='subtitle1'>Validated</Typography>
-                    <Stack
-                        direction="column"
-                        alignItems='center'
-                    >
-                        <Typography variant='subtitle2'>31 Jan 2022</Typography>
-                    </Stack>
-                    
-                </Stack>
+                {validated()}
                 <Divider />
-                <Stack
-                    direction="row"
-                    spacing={1}
-                    justifyContent='space-between'
-                    alignItems='center'
-                >
-                    <Stream />
-                    <Typography variant='subtitle1'>Planning</Typography>
-                    <Stack
-                        direction="column"
-                        alignItems='center'
-                    >
-                        <Typography variant='subtitle2'>Due</Typography>
-                        <Typography variant='subtitle2'>31 Jan 2022</Typography>
-                    </Stack>
-                </Stack>
+                {planned()}
                 <Divider />
-                <Stack
-                    direction="row"
-                    spacing={1}
-                    justifyContent='space-between'
-                    alignItems='center'
-                >
-                    <Pending />
-                    <Typography variant='subtitle1'>Implementation</Typography>
-                    <Stack
-                        direction="column"
-                        alignItems='center'
-                    >
-                        <Typography variant='subtitle2'>Due</Typography>
-                        <Typography variant='subtitle2'>31 Jan 2022</Typography>
-                    </Stack>
-                </Stack>
+                {implemented()}
                 <Divider />
-                <Stack
-                    direction="row"
-                    spacing={1}
-                    justifyContent='space-between'
-                    alignItems='center'
-                >
-                    <Pending />
-                    <Typography variant='subtitle1'>Completion</Typography>
-                    <Stack
-                        direction="column"
-                        alignItems='center'
-                    >
-                        <Typography variant='subtitle2'>Due</Typography>
-                        <Typography variant='subtitle2'>31 Jan 2022</Typography>
-                    </Stack>
-                </Stack>
+                {completed()}
                 <Divider />
             </Stack>
         </Paper>
