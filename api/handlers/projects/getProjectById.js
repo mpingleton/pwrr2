67f90a8a9c3485bddcc042a3e6c35d2cc2957718
Michael Pingleton <mnpingleton@gmail.com>
2022-09-24 16:15:42 +0300

    Adding sorting of stages based on sequence number.

--- api/handlers/projects/getProjectById.js
+++ api/handlers/projects/getProjectById.js
@@ -5,7 +5,18 @@ const getContactById = require('../../services/contacts/getContactById');
 
 module.exports = async (req, res) => {
     const projectData = await getProjectById(Number.parseInt(req.params.projectId));
+
     projectData.stages = await getStagesForProject(projectData.id);
+    projectData.stages.sort((a, b) => {
+        if (a.sequence < b.sequence) {
+            return -1;
+        }
+        else if (a.sequence > b.sequence) {
+            return 1;
+        }
+
+        return 0;
+    });
 
     const contactIds = await getContactsForProject(projectData.id);
     projectData.contacts = await Promise.all(contactIds.map((contact) => getContactById(contact.contactId)));
