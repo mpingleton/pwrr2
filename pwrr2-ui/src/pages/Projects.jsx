7856bf8c652a43bebd92ef537f465f7d37b6be7a
Michael Pingleton <mnpingleton@gmail.com>
2022-10-03 20:43:02 +0300

    Fixed padding.

--- pwrr2-ui/src/pages/Projects.jsx
+++ pwrr2-ui/src/pages/Projects.jsx
@@ -96,7 +96,8 @@ function Projects() {
                 width: 'fit-content',
                 top: '50%',
                 left: '50%',
-                transform: 'translate(-50%, -50%)'
+                transform: 'translate(-50%, -50%)',
+                padding: 2,
             }}
         >
             <Typography>
