0e57574afb8b2a04c22c11b5c6c2f71a0b8415f7
Michael Pingleton <mnpingleton@gmail.com>
2022-11-08 20:38:32 +0300

    Added a call to the database which retrieves the user who is assigned to the requested task, if there is one.

--- api/handlers/tasks/getTaskById.js
+++ api/handlers/tasks/getTaskById.js
@@ -1,6 +1,7 @@
 const getTaskById = require('../../services/tasks/getTaskById');
 const getGroupById = require('../../services/groups/getGroupById');
 const getProjectById = require('../../services/projects/getProjectById');
+const getSafeUserById = require('../../services/users/getSafeUserById');
 const getDependentTasksForTask = require('../../services/tasks/getDependentTasksForTask');
 const getIndependentTasksForTask = require('../../services/tasks/getIndependentTasksForTask');
 
@@ -9,6 +10,9 @@ module.exports = async (req, res) => {
     
     taskData.groupData = await getGroupById(taskData.groupId);
     taskData.projectData = await getProjectById(taskData.projectId);
+    if (taskData.userId !== null) {
+        taskData.userData = await getSafeUserById(taskData.userId);
+    }
     taskData.dependentTasks = await getDependentTasksForTask(taskData.id)
                                         .then((tasks) => Promise.all(tasks.map((task) => getTaskById(task))));
     taskData.independentTasks = await getIndependentTasksForTask(taskData.id)
