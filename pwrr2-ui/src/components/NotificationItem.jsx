b832e1059c5b592304251d7085faf1f5583ab663
Michael Pingleton <mnpingleton@gmail.com>
2022-11-13 15:01:25 +0300

    Added text handler for notifications about tasks.

--- pwrr2-ui/src/components/NotificationItem.jsx
+++ pwrr2-ui/src/components/NotificationItem.jsx
@@ -6,11 +6,39 @@ import {
 } from '@mui/material';
 
 function NotificationItem(props) {
+
+    const notifTitle = (action) => {
+        if (action === "") {
+            return "";
+        }
+        else if (action === "CANCEL_TASK") {
+            return "Task Cancelled";
+        }
+        else if (action === "COMPL_TASK") {
+            return "Task Completed";
+        }
+        else if (action === "NEW_TASK") {
+            return "New Task";
+        }
+        else if (action === "PAUSE_TASK") {
+            return "Task Paused";
+        }
+        else if (action === "RESUM_TASK") {
+            return "Task Resumed";
+        }
+        else if (action === "START_TASK") {
+            return "Task Started";
+        }
+        else {
+            return action;
+        }
+    };
+
     return (
         <ListItem button>
             <ListItemText
-                primary={props.notification.action}
-                secondary="Notification description goes here."
+                primary={notifTitle(props.notification.action)}
+                secondary={props.notification.taskId}
                 sx={{
                     textAlign: 'start'
                 }}
