98d8d2f617375d639f792a5b31bf38913af142ca
Michael Pingleton <mnpingleton@gmail.com>
2022-11-13 15:01:07 +0300

    Added notification to cancel task handler.

--- api/handlers/tasks/cancelTaskById.js
+++ api/handlers/tasks/cancelTaskById.js
@@ -1,6 +1,16 @@
 const cancelTaskById = require('../../services/tasks/cancelTaskById');
+const insertNotification = require('../../services/notifications/insertNotification');
 
 module.exports = async (req, res) => {
     const taskData = await cancelTaskById(req.params.taskId, req.user.id, new Date());
+
+    await insertNotification(
+        taskData.groupId,
+        taskData.projectId,
+        taskData.id,
+        "CANCEL_TASK",
+        new Date(),
+    );
+
     res.send(200, taskData);
 };
