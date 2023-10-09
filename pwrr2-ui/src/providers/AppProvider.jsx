8c9caf9051bb129d7e7f12ebbc3ffc3336c1ee87
Michael Pingleton <mnpingleton@gmail.com>
2023-10-09 13:33:36 -0400

    Made the switch to dark theme.

--- pwrr2-ui/src/providers/AppProvider.jsx
+++ pwrr2-ui/src/providers/AppProvider.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import PrimaryRouter from '../routes/PrimaryRouter';
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 function AppProvider() {
-    return (<PrimaryRouter />);
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <PrimaryRouter />
+        </ThemeProvider>
+    );
 }
 
 export default AppProvider;
